7024e11c017587166c73e93f86a1df5081007918
Nikos <niqos89@gmail.com>
2025-05-21 21:37:03 +0300

    feat: add reusable button component with props

---
+++ src/components/CounterButton.tsx
@@ -0,0 +1,21 @@
+type ButtonProps = {
+  onClick: () => void;
+  // ΔΗΛΩΝΩ ΟΤΙ ΕΙΝΑΙ OPTIONAL
+  disabled?: boolean;
+  label: string;
+  addClass?: string;
+}
+
+const CounterButton = ({onClick, disabled = false, label, addClass= "bg-black"}: ButtonProps) => {
+  return (
+    <>
+      <button className={"text-white py-2 px-4 disabled:bg-gray-600 " + addClass}
+              onClick={onClick}
+              disabled={disabled}>
+        {label}
+      </button>
+    </>
+  )
+}
+
+export default CounterButton;
