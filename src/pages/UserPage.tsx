d3c9ceee6e4f9c8e940eb5df97ce180990ae4134
Nikos <niqos89@gmail.com>
2025-06-09 22:48:39 +0300

    feat: use of "*" in params

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,6 +4,8 @@ import { useParams} from "react-router";
 const UserPage = () => {
 
   const {userId} = useParams();
+  // const params = useParams();
+  // const filePath = params["*"];
 
   useEffect(() => {
     document.title = `Cf7 user id: ${userId}`;
